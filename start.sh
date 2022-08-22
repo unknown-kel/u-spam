8d086421ad4f0665bb5290f1b350c7a3c8924728
unknown-kel <57179329+unknown-kel@users.noreply.github.com>
2022-08-22 07:41:37 -0700

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,14 @@
+g="\033[1;32m"
+r="\033[1;31m"
+b="\033[1;34m"
+w="\033[0m"
+o="\033[1;33m"
+
+cd
+cd
+echo -e $w"["$g"INFO"$w"]"$b" Installing Requirements .."$w
+pip install requests
+pip install pystyle
+cd u-clone
+echo -e $w"["$g"INFO"$w"]"$b"Requirements Installed..."$w
+echo -e $w"["$g"INFO"$w"]"$b"Usage : python. spam.py.."$w
